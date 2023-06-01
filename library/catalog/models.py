246ee17e0413bb9468ea20ec204e1c4b4ad33291
jeremiah ojo <ojoj665@gmail.com>
2023-06-01 16:37:15 +0100

    setup + models

---
+++ library/catalog/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.urls import reverse
+import uuid
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+        
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField(null=True, blank=True)
+   
+    class Meta:  #This for the admin panel, we want it ordered that way
+        ordering =['last_name', 'first_name']
+
+    def get_absolute_urls(self): # This returns the url that access a particular author detail
+        return reverse('author_detail', kwargs={"pk":self.pk})
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+    
+class Language(models.Model):
+    name= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=200)
+    isbn= models.CharField('ISBN', max_length=13, unique=True)
+    genre = models.ManyToManyField(Genre)
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True )
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_urls(self):    
+        return reverse('book_detail', kwargs={"pk":self.pk})
+    
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On Loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m')
+
+    class Meta :
+        ordering= ['due_back']
+
+    def __str__(self):
+        return f"{self.id} {self.book.title}"
